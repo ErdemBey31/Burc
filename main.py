ed7908a7751a2ac51ac6003fba1336bf23c2bddf
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-10-08 10:42:44 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from burclar import burclar
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return '<h1 style="color:green">Sitenin sonuna bir burç girmeniz gerekiyor.</h1>'
+    
+@app.route("/oglak")
+def oglak():
+    return burclar.oglak('gunluk')
+
+@app.route("/koc")
+def koc():
+    return burclar.koc('gunluk')
+
+@app.route("/boga")
+def boga():
+    return burclar.boga('gunluk') 
+
+@app.route("/ikizler")
+def ikizler():
+    return burclar.ikizler('gunluk')
+
+
+@app.route("/yengec")
+def yengec():
+    return burclar.yengec('gunluk')
+
+@app.route("/aslan")
+def aslan():
+    return burclar.aslan('gunluk')
+
+@app.route("/basak")
+def basak():
+    return burclar.basak('gunluk')
+
+@app.route("/terazi")
+def terazi():
+    return burclar.terazi('gunluk')
+
+
+@app.route("/akrep")
+def akrep():
+    return burclar.akrep('gunluk')
+
+@app.route("/yay")
+def yay():
+    return burclar.yay('gunluk')
+
+@app.route("/kova")
+def kova():
+    return burclar.kova('gunluk')
+
+@app.route("/balik")
+def balik():
+    return burclar.balik('gunluk')
+
+
+if __name__ == '__main__':
+    app.run()
